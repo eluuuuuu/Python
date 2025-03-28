69a13914a3f5e7ec2aed32760197bac42e06aa4c
elias abdo <eliasabdoo@yahoo.co.uk>
2025-03-28 20:15:30 +0000

    created a new file

---
+++ password.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choices(characters, k=length))
+    return password
+
+print(generate_password(16))
